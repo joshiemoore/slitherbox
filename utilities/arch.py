8c3433f7cc0507d49acf987ec663b93923d80c7f
joshiemoore <jxm5210@g.rit.edu>
2022-07-16 17:04:12 -0400

    Add arch

---
+++ utilities/arch.py
@@ -0,0 +1,14 @@
+"""
+arch
+
+Print the hardware architecture to standard output.
+"""
+
+import platform
+
+
+def main(args):
+    if len(args) != 0:
+        print('Usage: arch')
+        return 1
+    print(platform.machine())
