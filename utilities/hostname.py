d4f97af954e6e0cb56fb885af90d7c55c80121ad
joshiemoore <jxm5210@g.rit.edu>
2022-07-16 16:57:07 -0400

    Add hostname

---
+++ utilities/hostname.py
@@ -0,0 +1,14 @@
+"""
+hostname
+
+Print the name of the host to standard output.
+"""
+
+import platform
+
+
+def main(args):
+    if len(args) != 0:
+        print('Usage: hostname')
+        return 1
+    print(platform.node())
