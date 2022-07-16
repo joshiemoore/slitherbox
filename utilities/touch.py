8f894001f2e93ae2e6a05cb360d164cfb75c9dbf
joshiemoore <jxm5210@g.rit.edu>
2022-07-16 01:47:56 -0400

    Add touch

---
+++ utilities/touch.py
@@ -0,0 +1,19 @@
+"""
+touch
+
+Create a file, or update an existing file's accessed and modified timestamps.
+"""
+
+import os
+import argparse
+
+
+def main(args):
+    parser = argparse.ArgumentParser(description='Create a file, or update a file\'s timestamps.')
+    parser.add_argument('file', type=str)
+    _args = parser.parse_args(args=args)
+
+    try:
+        os.utime(_args.file, None)
+    except OSError:
+        open(_args.file, 'a').close()
